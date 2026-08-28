7f021171f89e9fba4d1e92314fe8a0e8cb377bfb
rtwk3 <rtwk03@gmail.com>
2026-08-28 15:57:17 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+# pyrefly: ignore [missing-import]
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api/data", methods=["GET"])
+def get_data():
+    return jsonify({
+        "name": "Ruthwik",
+        "age": 22,
+        "city": "Guntur",
+        "skills": ["Python", "Flask", "HTML", "CSS", "JavaScript"]
+    })
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
